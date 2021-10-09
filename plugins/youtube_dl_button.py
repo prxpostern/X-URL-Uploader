0ba1a9ee3b047457a3001a98c0048fcd5398d18c
prxpostern <44132439+prxpostern@users.noreply.github.com>
2021-10-09 14:43:05 +0330

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -301,6 +301,8 @@ async def youtube_dl_call_back(bot, update):
             elif tg_send_type == "video":
                 probe = await ffprobe.func(download_directory)
                 video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
+                width = int(video_stream['width'] if 'width' in video_stream else 0)
+                height = int(video_stream['height'] if 'height' in video_stream else 0)
                 thumbnail = await thumbnail_video.func(download_directory)
                 await bot.send_video(
                     chat_id=update.message.chat.id,
